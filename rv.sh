6b6bf627400c3b57991b9a05fbad358e7814b9a8
yiyiwang <shd101wyy@sina.com>
2018-08-15 16:19:38 -0500

    Initial commit

---
+++ rv.sh
@@ -0,0 +1,20 @@
+set -x # Print debug info
+
+# Install necessary packages
+sudo apt-get install libssl-dev libcap-dev
+
+# Build the project
+CC=cc
+LD=ld
+mkdir build-atf; cd build-atf; ../unit/atf-src/configure --enable-tools --disable-shared --prefix=/tmp/atf; make -j20; make install; cd ../
+CC=rvpc LD=rvpc ./configure --with-atf=/tmp/atf
+make -C ./lib/dns gen BUILD_CC=cc # Avoid hang
+make -j`nproc`
+
+# Tests
+report_path=`pwd`/report
+/tmp/atf/bin/atf-run ./lib/dns/tests/dispatch_test
+cd ./bin/tests/system/smartsign; sh setup.sh; RVP_ANALYSIS_ARGS="--html-dir $report_path" sh tests.sh
+
+# Upload report
+rv-upload-report $report_path
